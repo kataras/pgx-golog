a4a09ba42db79668654839ad972507be1d6ac797
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2023-06-24 23:15:05 +0300

    minor

--- adapter.go
+++ adapter.go
@@ -14,6 +14,41 @@ type Logger struct {
 }
 
 // NewLogger returns a new logger which implements the pgx's tracelog's logger interface.
+//
+// Example Code:
+//
+//	import (
+//		"context"
+//
+//		"github.com/jackc/pgx/v5/pgxpool"
+//		"github.com/jackc/pgx/v5/tracelog"
+//		"github.com/kataras/golog"
+//		pgxgolog "github.com/kataras/pgx-golog"
+//	)
+//
+//	func main() {
+//		golog.SetLevel("debug")
+//
+//		logger := pgxgolog.NewLogger(golog.Default)
+//		tracer := &tracelog.TraceLog{
+//			Logger:   logger,
+//			LogLevel: tracelog.LogLevelTrace,
+//		}
+//
+//		connString := "postgres://postgres:admin!123@localhost:5432/test_db?sslmode=disable&search_path=public"
+//		connConfig, err := pgxpool.ParseConfig(connString)
+//		if err != nil {
+//			panic(err)
+//		}
+//		connConfig.ConnConfig.Tracer = tracer
+//
+//		pool, err := pgxpool.NewWithConfig(context.Background(), connConfig)
+//		if err != nil {
+//			panic(err)
+//		}
+//
+//		// [...]
+//	}
 func NewLogger(l *golog.Logger) *Logger {
 	return &Logger{l: l}
 }
@@ -24,27 +59,28 @@ func (l *Logger) Log(ctx context.Context, level tracelog.LogLevel, msg string, d
 
 	switch level {
 	case tracelog.LogLevelTrace:
-		if len(data) > 0 {
-			data["PGX_LOG_LEVEL"] = level
-		} else {
-			data = map[string]interface{}{"PGX_LOG_LEVEL": level}
+		if data == nil {
+			data = make(map[string]interface{})
 		}
+
+		data["PGX_LOG_LEVEL"] = level
 
 		logger.Debug(msg, golog.Fields(data))
 	case tracelog.LogLevelDebug:
-		logger.Debug(msg)
+		logger.Debug(msg, golog.Fields(data))
 	case tracelog.LogLevelInfo:
-		logger.Info(msg)
+		logger.Info(msg, golog.Fields(data))
 	case tracelog.LogLevelWarn:
-		logger.Warn(msg)
+		logger.Warn(msg, golog.Fields(data))
 	case tracelog.LogLevelError:
-		logger.Error(msg)
+		logger.Error(msg, golog.Fields(data))
 	default:
-		if len(data) > 0 {
-			data["INVALID_PGX_LOG_LEVEL"] = level
-		} else {
-			data = map[string]interface{}{"INVALID_PGX_LOG_LEVEL": level}
+		if data == nil {
+			data = make(map[string]interface{})
 		}
+
+		data["INVALID_PGX_LOG_LEVEL"] = level
+
 		logger.Error(msg, golog.Fields(data))
 	}
 }
